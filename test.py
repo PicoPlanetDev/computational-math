701f289016032b11a65e43698bcc5ceb4c4e13b7
PicoPlanetDev <picoplanetdev@gmail.com>
2021-10-01 08:09:14 -0400

    Started work on linearRegression

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import math
 
-def moveToBack(a,b):
-    for i in range(len(b)):
-        for j in range(len(a)):
-            if a[j] == b[i]:
-                a.remove(a[j])
-                a.append(b[i])
-    return a
+# Returns two lists of X and Y values from a combined list of X and Y values
+def splitToXY(L):
+    xList = L[::2]
+    yList = L[1::2]
+    return xList, yList
 
-print(moveToBack([2, 3, 3, 4, 1, 5], [3]))
+def linearRegression(L):
+    xList, yList = splitToXY(L)
+    sumX, sumY = sum(xList), sum(yList)
+    n = len(xList)
+    sumXY = sum([xList[i] * yList[i] for i in range(n)])
+    sumX2 = sum(xList[i]**2 for i in range(n))
+    slope = (sumX * sumY - (n * sumXY)) / ((sumX**2) - (n * sumX2))
+    offset = (sumX * sumXY - (sumY * sumX2)) / ((sumX**2) - (n * sumX2))
+    return slope, offset
+
+print(linearRegression([0,2,1,3,2,4]))
--- unit-3/sigmondUnitThree.py
+++ unit-3/sigmondUnitThree.py
@@ -145,8 +145,21 @@ def moveToBack(a,b):
                 a.append(b[i])
     return a
 
+# Returns two lists of X and Y values from a combined list of X and Y values
+def splitToXY(L):
+    xList = L[::2]
+    yList = L[1::2]
+    return xList, yList
+
 def linearRegression(L):
-    return None
+    xList, yList = splitToXY(L)
+    sumX, sumY = sum(xList), sum(yList)
+    n = len(xList)
+    sumXY = sum([xList[i] * yList[i] for i in range(n)])
+    sumX2 = sum(xList[i]**2 for i in range(n))
+    slope = (sumX * sumY - (n * sumXY)) / ((sumX**2) - (n * sumX2))
+    offset = (sumX * sumXY - (sumY * sumX2)) / ((sumX**2) - (n * sumX2))
+    return slope, offset
 
 def exponentialRegression(L):
     return None
